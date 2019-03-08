ba075459650bb2a8afc2daa254a0f7a7c6ae4d47
Krikor Ailanjian <kailanjian@gmail.com>
2019-03-08 12:39:38 -0800

    working a lot better

--- inverse.s
+++ inverse.s
@@ -1,12 +1,12 @@
 //program 1, multiplicative inverse
 //getting the values out of memory
 mov #8      //move memloc 8 into acc
-wrt r7      //write that value into r8
-ldr [r7]    //load memloc[r8] into acc
+wrt r7      //write that value into r6
+ldr [r7]    //load memloc[r6] into acc
 wrt r0      //write MSB to r0
 mov #9      //move memloc 9 into acc
-wrt r7      //write that value into r8
-ldr [r7]    //load memloc[r8] into acc
+wrt r7      //write that value into r6
+ldr [r7]    //load memloc[r6] into acc
 wrt r1      //write LSB to r1
 
 //check for zero
@@ -28,7 +28,7 @@ br          //becasue of 4 bit signed limit
 mov #0xff   //load max value into acc
 str [r7]    //load max value into memloc #9
 mov #8      //load memloc 8 into acc
-wrt r7      //load memloc 8 into r8
+wrt r7      //load memloc 8 into r6
 mov #0xff   //load max value into acc
 str [r7]    //load max value into memloc #8
 
@@ -47,13 +47,13 @@ wrt r4      //save to r4
 mov #0x00  //r5 is going to count the number of times r4 reaches zero
 wrt r5      //will stop the loop when r > 1
 
-mov #0     //set r7 | r8 to be zero
+mov #0     //set r7 | r6 to be zero
 wrt r7
-wrt r8
+wrt r6
 
 //INPUT(R)   : r0 | r1
 //DIVISOR(N) : r2 | r3
-//RESULT(Q)  : r7 | r8
+//RESULT(Q)  : r7 | r6
 //Q_SETTER   : r4
 //COUNTER    : r5
 
@@ -65,31 +65,21 @@ bne #4     //skip the lsb check
 
 rdr r1      //LSB{R}
 cmp r3      //LSB{N}
-ble #8     //skip the else step
+ble #5      //if LSB{R} <= LSB{N} skip the else step
 mov #4
 br
 
 rdr r0      //MSB{R}
 cmp r2      //MSB{N}
-ble #3      //skip the else step
+ble #3      //if MSB{R} <= MSB{R} skip the else step
 
-//else body, won't do anything cuz r7 | r8 are zero by default
-mov #29    //skip to shift
+//else body, won't do anything cuz r7 | r6 are zero by default
+mov #18    //skip to shift
 br          //byeeeeee
-
-rdr r4
-cmp $ZERO   //is r4 zero? gotta reset it if it is
-bne #6
-
-mov #1     //increment
-add r5      //r4
-wrt r5
-mov #0x80  //reset r4
-wrt r4
 
 mov #0     //compare r5 against 0
 cmp r5      //if !=, then we work on LSB
-bne #7     //skip over MSB setting
+bne #6     //skip over MSB setting
 
 rdr r4
 orr r7
@@ -98,13 +88,10 @@ mov #4
 br         //skip over LSB setting
 
 rdr r4
-orr r8
-wrt r8
+orr r6
+wrt r6
 
 //done with MSB/LSB setting
-rdr r4
-lsr #1
-wrt r4
 
 //N := N - R
 //N[0] := N[0] - R[0]
@@ -117,6 +104,7 @@ rdr r2
 sbc r0
 wrt r2
 
+// SHIFTING HERE, END IF
 //N << 1
 rdr r2      //r2 << 1
 lsl #1     //r2 << 1
@@ -131,6 +119,31 @@ rdr r3      // r3 << 1
 lsl #1     // r3 << 1
 wrt r3      // r3 << 1
 
+// q_ setter>> 1 to be used in next loop
+rdr r4
+lsr #1
+wrt r4
+
+rdr r4
+cmp $ZERO   //is r4 zero? gotta reset it if it is
+bne #6
+
+mov #1     //increment
+add r5      //r4
+wrt r5
+mov #0x80  //reset r4
+wrt r4
+
+// if counter > 1 (or 1 < counter, or 2 <= counter) exit loop
+mov #2
+cmp r5
+ble #3 // exit loop
+
+mov #-56 // TODO loop
+br
+
+// loop end
+// write to memory the results
 mov #10
 wrt r0
 rdr r7
@@ -138,8 +151,9 @@ str [r0]
 
 mov #11
 wrt r0
-rdr r8
+rdr r6
 str [r0]
 
 halt
 halt
+
